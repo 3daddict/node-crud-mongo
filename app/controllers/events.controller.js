7452c8cdd310e9e3c0c286882ca8256cd64422a2
Michael Salvati <print@3daddict.com>
2018-11-01 15:35:12 -0700

    event routing created and event skeleton with dummy data

---
+++ app/controllers/events.controller.js
@@ -0,0 +1,12 @@
+module.exports = {
+    showEvents: (req, res) => {
+        //create dummy data
+        const events = [
+            { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket yo' },
+            { name: 'Swimming', slug: 'swimming', description: 'Michael Phelps is fast' },
+            { name: 'Weight Lifting', slug: 'weightlifting', description: 'Liftinh heavy things up' }
+        ];
+        //return a view with data
+        res.render('pages/events', { events: events });
+    }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,15 @@
 //express router
 const express = require('express'),
     router = express.Router(),
-    mainController = require('./controllers/main.controllers');
+    mainController = require('./controllers/main.controllers'),
+    eventController = require('./controllers/events.controller');
 
 //export router
 module.exports = router;
 
 //define routes
+//main routes
 router.get('/', mainController.showHome);
+
+//event routes
+router.get('/events', eventController.showEvents);
